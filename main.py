1267ea5e867bb2fb59ad77a5a563d9c27688a69e
yesa0798 <147570621+yesa0798@users.noreply.github.com>
2025-02-20 16:27:46 +0700

    Menambahkan fungsi kalkulator dasar (perkalian dan pembagian)
    Menambahkan fungsi untuk operasi matematika dasar yaitu perkalian dan pembagian ke dalam program kalkulator sederhana. 
    Pada fungsi pembagian, ditambahkan pengecekan pembagi agar tidak terjadi pembagian dengan nol. Kemudian, hasil dari masing - masing operasi ditampilkan melalui fungsi main() untuk memverifikasi fungsionalitas kalkulator.

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 #tolong lengkapi fungsi python berikut untuk membuat kalkulator sederhana
 #buat dengan format yang sama
 
+def penambahan(a, b):
+    total = a + b
+    return total
 
-def penambahan(a,b):
-	total = a+b
-	return(total)
+def pengurangan(a, b):
+    total = a - b
+    return total
+
+def perkalian(a, b):
+    total = a * b
+    return total
+
+def pembagian(a, b):
+    if b != 0:
+        total = a / b
+        return total
+    else:
+        return "Error: Pembagi tidak boleh 0"
 
 def main():
-	
-	print(penambahan(10,5))
+    print("Penambahan: ", penambahan(10, 5))
+    print("Pengurangan: ", pengurangan(10, 5))
+    print("Perkalian: ", perkalian(10, 5))
+    print("Pembagian: ", pembagian(10, 5))
+    print("Pembagian dengan 0: ", pembagian(10, 0))
 
 main()
