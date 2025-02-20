48b84c1e3f5decc85248a9a009336201478e0204
Norby16 <snorbertansel16@gmail.com>
2025-02-20 16:24:10 +0700

    Update main.py
    penambahan pembagian

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #tolong lengkapi fungsi python berikut untuk membuat kalkulator sederhana
 #buat dengan format yang sama
 
-
+def pembagian(a,b):
+	total = a/b
+	return(total)
+	
 def penambahan(a,b):
 	total = a+b
 	return(total)
@@ -11,7 +14,7 @@ def pengurangan(a,b):
 	return(total)
 
 def main():
-	
+	print(pembagian(20,5))
 	print(penambahan(10,5))
 	print(pengurangan(1,2))
 
