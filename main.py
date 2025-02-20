918c5bb0d1a53cdcae0d2a125b593b07bb8415ea
Gendis27 <gendisangeltri2711@gmail.com>
2025-02-20 16:31:22 +0700

    Dapatkan Iphone dengan klik title ini!!!.py
    Tapi boonk :v

--- main.py
+++ main.py
@@ -11,10 +11,15 @@ def penambahan(a,b):
 def pengurangan(a,b):
 	total = a-b
 	return(total)
+def pembagian(a,b):
+	total = a/b
+	return(total)
+
 
 def main():
 	print(perkalian(10,10))	
 	print(penambahan(10,5))
 	print(pengurangan(1,2))
-
+	print(pembagian(1,2))
+	
 main()
