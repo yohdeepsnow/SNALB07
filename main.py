e9f306f18e5c56811695510ddeb37951dcb5704e
unknown <marvellbudiman@gmail.com>
2025-02-20 16:04:15 +0700

    Penambahan Pengurangan

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ def penambahan(a,b):
 	total = a+b
 	return(total)
 
+def pengurangan(a,b):
+	total = a-b
+	return(total)
+
 def main():
 	
 	print(penambahan(10,5))
+	print(pengurangan(1,2))
 
 main()
