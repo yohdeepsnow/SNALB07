5076789a3a0b0edb47d9a158edf241598fe6c5e8
Norby16 <snorbertansel16@gmail.com>
2025-02-20 15:48:03 +0700

    Update main.py
    ditambahkan pengurangan

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 #tolong lengkapi fungsi python berikut untuk membuat kalkulator sederhana
 #buat dengan format yang sama
 
+def pengurangan(a,b):
+	total = a+b
+	return(total)
 
 def penambahan(a,b):
 	total = a+b
 	return(total)
 
 def main():
-	
+	print(pengurangan(20,15)
 	print(penambahan(10,5))
 
 main()
