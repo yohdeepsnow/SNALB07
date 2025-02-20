ee17359275ab643ebe64c26fcf77c677074dbc7d
yohdeepsnow <muliono.yohan@gmail.com>
2025-02-20 08:41:41 +0000

    menambahkan file python main.py untuk membuat kalkulator sederhana

---
+++ main.py
@@ -0,0 +1,13 @@
+#tolong lengkapi fungsi python berikut untuk membuat kalkulator sederhana
+#buat dengan format yang sama
+
+
+def penambahan(a,b):
+	total = a+b
+	return(total)
+
+def main():
+	
+	print(penambahan(10,5))
+
+main()
