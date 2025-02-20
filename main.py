014d17d43f0b49c8432b14b4877b3dfafca66694
ane existensial life crisis <mariannesoedu@gmail.com>
2025-02-20 16:27:48 +0700

    halo ko ini ane
    hehe

--- main.py
+++ main.py
@@ -10,9 +10,14 @@ def pengurangan(a,b):
 	total = a-b
 	return(total)
 
+def perkalian(a,b):
+	total = a*b
+	return(total)
+	
 def main():
 	
 	print(penambahan(10,5))
 	print(pengurangan(1,2))
-
+	print(perkalian(10,5))
+	
 main()
