e91c7dbf467479936c379acaa29edf5b7fee0b0e
Jonathan-BINUS <jonathan.rahadian@binus.ac.id>
2025-02-20 16:00:54 +0700

    Update and rename main.py to main(1).py

--- main(1).py
+++ main(1).py
@@ -5,9 +5,11 @@
 def penambahan(a,b):
 	total = a+b
 	return(total)
-
+def pengurangan(c,d):
+	total = c-d
+	return(total)
 def main():
 	
 	print(penambahan(10,5))
-
+	print(pengurangan(100,5))
 main()
