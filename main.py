cfea588de7419e30e28a3eeafb18e264d3adcfea
Dazaiish <khomeini6b@gmail.com>
2025-02-20 16:34:47 +0700

    info monster hunter wilds (atau rise)
    penambahan pembagian ke fungsi

--- main.py
+++ main.py
@@ -12,9 +12,14 @@ def pengurangan(a,b):
 	total = a-b
 	return(total)
 
+def pembagian(a,b):
+	total = a/b
+	return(total)
+
 def main():
 	print(perkalian(10,10))	
 	print(penambahan(10,5))
 	print(pengurangan(1,2))
+	print(pembagian(4,2))
 
 main()
